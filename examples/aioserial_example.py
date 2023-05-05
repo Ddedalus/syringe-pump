0f51a1d7ed04acca0b2d01ffb8b50a11a62f1d3c
Hubert Bereś <h.beres@hotmail.com>
2023-05-05 19:11:52 +0100

    Working the aio client

--- examples/aioserial_example.py
+++ examples/aioserial_example.py
@@ -2,52 +2,77 @@ import asyncio
 import time
 
 import aioserial
-from pydantic import BaseModel
+from pydantic import BaseModel, PrivateAttr
+
+XON = b"\x11"
 
 
 class Pump(BaseModel):
+    """High-level interface for the Legato 100 syringe pump.
+    Upon initialisation, sets poll mode to on, making prompts parsable.
+    """
+
     serial: aioserial.AioSerial
 
     class Config:
         arbitrary_types_allowed = True
 
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.serial.write(b"poll on")
+        output = self.serial.read_until(XON)
+        # TODO: verify the output
+
+    async def _write(self, command: str):
+        return await self.serial.write_async((command + "\r\n").encode())
+
     async def start(self):
-        await self.serial.write_async(b"run\r\n")
+        await self._write("run")
+        return await self.parse_prompt()
 
     async def stop(self):
-        await self.serial.write_async(b"stop\r\n")
+        await self._write("stop")
+
+    async def parse_prompt(self) -> str:
+        if self._poll_mode:
+            output = await self.serial.read_until_async(XON)
+            output.rstrip(XON).lstrip(b"\n")
+            return output.decode()
+
+        output = b""
+        while True:
+            char = await self.serial.read_async()
+            output += char
+            if char == b"\n":
+                continue
+            if char in b":><":
+                return output.decode()
+            elif char == "T":
+                output += await self.serial.read_async()
+                return output.decode()
+            else:
+                raise ValueError(f"Unknown prompt: {output.decode()}")
 
     async def read_line(self):
-        output = (await self.serial.readline_async()).decode(errors="ignore")
-        print(len(output), output.strip(), flush=True)
+        bytes = await self.serial.read_until_async(expected=b"\n")
+        output = bytes.decode(errors="ignore")
+        print(len(output), bytes, flush=True)
 
 
 async def main(pump: Pump):
     start = time.time()
-    await pump.start()
+    print(await pump.pool(enabled=True))
+    print(time.time() - start, "pool on")
+    print(await pump.start())
     print(time.time() - start, "start")
     await asyncio.sleep(1)
     print(time.time() - start, "sleep")
-    await pump.read_line()
-    print(time.time() - start, "line 1")
-    await pump.read_line()
-    print(time.time() - start, "line 2")
-    try:
-        await pump.read_line()
-    except Exception as e:
-        print(e)
-    print(time.time() - start, "line 3")
     await pump.stop()
     print(time.time() - start, "stop")
 
 
 if __name__ == "__main__":
-    serial = aioserial.AioSerial(
-        port="COM4",
-        baudrate=9600,
-        cancel_read_timeout=0.5,
-        cancel_write_timeout=0.5,
-        timeout=3,
-    )
+    serial = aioserial.AioSerial(port="COM4", baudrate=115200, timeout=2)
     pump = Pump(serial=serial)
+    assert pump._poll_mode is False
     asyncio.run(main(pump))
