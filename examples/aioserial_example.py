6d0f597cefa31ae6dc4244232eac7c06a9664600
Hubert Bereś <h.beres@hotmail.com>
2023-05-05 19:14:34 +0100

    Working aio example

--- examples/aioserial_example.py
+++ examples/aioserial_example.py
@@ -19,7 +19,7 @@ class Pump(BaseModel):
 
     def __init__(self, **data):
         super().__init__(**data)
-        self.serial.write(b"poll on")
+        self.serial.write(b"poll on\r\n")
         output = self.serial.read_until(XON)
         # TODO: verify the output
 
@@ -34,35 +34,13 @@ class Pump(BaseModel):
         await self._write("stop")
 
     async def parse_prompt(self) -> str:
-        if self._poll_mode:
-            output = await self.serial.read_until_async(XON)
-            output.rstrip(XON).lstrip(b"\n")
-            return output.decode()
-
-        output = b""
-        while True:
-            char = await self.serial.read_async()
-            output += char
-            if char == b"\n":
-                continue
-            if char in b":><":
-                return output.decode()
-            elif char == "T":
-                output += await self.serial.read_async()
-                return output.decode()
-            else:
-                raise ValueError(f"Unknown prompt: {output.decode()}")
-
-    async def read_line(self):
-        bytes = await self.serial.read_until_async(expected=b"\n")
-        output = bytes.decode(errors="ignore")
-        print(len(output), bytes, flush=True)
+        output = await self.serial.read_until_async(XON)
+        output.rstrip(XON).lstrip(b"\n")
+        return output.decode()
 
 
 async def main(pump: Pump):
     start = time.time()
-    print(await pump.pool(enabled=True))
-    print(time.time() - start, "pool on")
     print(await pump.start())
     print(time.time() - start, "start")
     await asyncio.sleep(1)
@@ -74,5 +52,4 @@ async def main(pump: Pump):
 if __name__ == "__main__":
     serial = aioserial.AioSerial(port="COM4", baudrate=115200, timeout=2)
     pump = Pump(serial=serial)
-    assert pump._poll_mode is False
     asyncio.run(main(pump))
