53777aafebe5ca1831c9f0ee7b46ce2517fc535b
Hubert Bereś <h.beres@hotmail.com>
2023-05-05 19:17:15 +0100

    Extract the client

--- examples/aioserial_example.py
+++ examples/aioserial_example.py
@@ -2,41 +2,8 @@ import asyncio
 import time
 
 import aioserial
-from pydantic import BaseModel, PrivateAttr
 
-XON = b"\x11"
-
-
-class Pump(BaseModel):
-    """High-level interface for the Legato 100 syringe pump.
-    Upon initialisation, sets poll mode to on, making prompts parsable.
-    """
-
-    serial: aioserial.AioSerial
-
-    class Config:
-        arbitrary_types_allowed = True
-
-    def __init__(self, **data):
-        super().__init__(**data)
-        self.serial.write(b"poll on\r\n")
-        output = self.serial.read_until(XON)
-        # TODO: verify the output
-
-    async def _write(self, command: str):
-        return await self.serial.write_async((command + "\r\n").encode())
-
-    async def start(self):
-        await self._write("run")
-        return await self.parse_prompt()
-
-    async def stop(self):
-        await self._write("stop")
-
-    async def parse_prompt(self) -> str:
-        output = await self.serial.read_until_async(XON)
-        output.rstrip(XON).lstrip(b"\n")
-        return output.decode()
+from syringe_pump import Pump
 
 
 async def main(pump: Pump):
